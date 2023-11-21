bf740aa234db81450cfa28c3cde37bb8deca9e39
Thiago de Souza <thiagodesouzarb@gmail.com>
2023-11-20 22:03:33 -0300

    conversor de moeda com membro estatico

---
+++ MembroEstaticoExercicio/ConversorDeMoedas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembroEstaticoExercicio
+{
+    internal class ConversorDeMoedas
+    {
+        public static double Iof = 6.0;
+
+        public static double ConversorDolar(double cotacao , double quantia)
+        {
+            double total = cotacao  * quantia;
+            return total;
+        }
+        public static double ConversorEuro(double cotacao, double quantia)
+        {
+            double total = cotacao * quantia;
+            return total;
+        }
+        public static double ConversorLibra(double cotacao, double quantia)
+        {
+            double total = cotacao * quantia;
+            return total;
+        }
+        public static double ConversorYuan(double cotacao, double quantia)
+        {
+            double total = cotacao * quantia;
+            return total;
+        }
+       
+    }
+}
---
+++ MembroEstaticoExercicio/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MembroEstaticoExercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual moeda quer converter?");
+            Console.WriteLine(" - Euro");
+            Console.WriteLine(" - Dolar");
+            Console.WriteLine(" - Libra");
+            Console.WriteLine(" - Yuan");
+            Console.WriteLine(" - Fim");
+            Console.Write("Insira a moeda para a conversão: ");
+            string moeda = Console.ReadLine();
+            string nomemoeda = moeda.ToLower();
+
+            while (nomemoeda != "FIM")
+            {
+                Console.Write($"Insira a cotação atual do(a) {nomemoeda}: ");
+                double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write($"Insira quantas unidades de {nomemoeda} você irá comprar: ");
+                double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double valorfinal = 0;
+     
+                switch (nomemoeda)
+                {
+                    case "euro": 
+                        valorfinal = ConversorDeMoedas.ConversorEuro(cotacao, quantia);
+                        break;
+
+                    case "dolar": 
+                        valorfinal = ConversorDeMoedas.ConversorDolar(cotacao, quantia);
+                        break;
+
+                    case "dólar":
+                        valorfinal = ConversorDeMoedas.ConversorDolar(cotacao, quantia);
+                        break;
+
+                    case "libra": 
+                        valorfinal = ConversorDeMoedas.ConversorLibra(cotacao, quantia);
+                        break; 
+
+                    case "yuan": 
+                        valorfinal = ConversorDeMoedas.ConversorYuan(cotacao, quantia);
+                        break;
+                }
+
+                Console.WriteLine("Valor a ser pago em reais:  " + valorfinal.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.Write("Insira a moeda para a conversão: ");
+                moeda = Console.ReadLine();
+            }
+
+        }
+    }
+}
